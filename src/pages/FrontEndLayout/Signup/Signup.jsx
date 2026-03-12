c52d18273b6856a145f5f5d451838bbf2fc32670
James <ohbwgab18@gmail.com>
2026-03-12 23:28:12 +0800

    render SET

--- src/pages/FrontEndLayout/Signup/Signup.jsx
+++ src/pages/FrontEndLayout/Signup/Signup.jsx
@@ -13,7 +13,7 @@ import longinSlider03 from "../../../assets/images/common/login-slider-03.png";
 
 import { toast } from "react-toastify";
 
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE;
 
 export default function Signup() {
   const navigate = useNavigate();
